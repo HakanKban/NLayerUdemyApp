74a1ce825c6ce41f455ebb9fcf25cc4b153a4655
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2022-05-23 23:47:30 +0300

    adding migration

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository;
+using NLayer.Repository.Repositories;
+using NLayer.Repository.Unitofwork;
+using System.Reflection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +14,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUnitofwork, Unitofwork>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+builder.Services.AddDbContext<AppDbContext>(x=>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"),option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name); // Migrationýn bulunduðu assembly i iþaret ettik. appsetting.json'da baðlantý cümleciði.
+    });
+});
 
 var app = builder.Build();
 
