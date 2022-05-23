a0dbbc8ea2e415354ef402d3f241b9bfbfc78716
Hakan Kaban <90520449+HakanKban@users.noreply.github.com>
2022-05-23 20:51:12 +0300

    Add project files.

---
+++ NLayer.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Core.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+       
+        Task<T> GetByIdAsync(int id);
+        IQueryable<T> GetAll(Expression<Func<T,bool>> expression);
+        IQueryable<T> Where(Expression<Func<T, bool>> expression);
+        Task<bool> AnyAsync(Expression<Func<T, bool>> expression); //Daha fazla performans sağlar, veri tabanına yapılacak olan sorguyu oluşturuyor.
+        Task AddAsync(T entity); 
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Remove(T entity); // asenkron metodu yok çünkü uzun süren bir işlem değil.
+        void RemoveRange(IEnumerable<T> entities);
+        void  UpdateAsync(T entity);
+    }
+}
